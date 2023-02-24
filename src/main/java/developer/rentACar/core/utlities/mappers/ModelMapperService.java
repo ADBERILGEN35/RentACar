83b01ba272f6cab659e119453f1c8d61b3348a3d
ADBERILGEN35 <91200001205@ogrenci.ege.edu.tr>
2023-02-24 20:18:58 +0300

    Added Mapper

---
+++ src/main/java/developer/rentACar/core/utlities/mappers/ModelMapperService.java
@@ -0,0 +1,9 @@
+package developer.rentACar.core.utlities.mappers;
+
+import org.modelmapper.ModelMapper;
+
+public interface ModelMapperService {
+    ModelMapper forResponse();
+
+    ModelMapper forRequest();
+}
